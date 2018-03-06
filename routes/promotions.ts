a0ee94719a2e938e46c6c262f46b5837497d0b89
Nam Truong Pham <>
2018-03-06 11:42:15 +0700

    ++ Fix

--- routes/promotions.ts
+++ routes/promotions.ts
@@ -254,7 +254,7 @@ router.get('/', _.validQuery(queries), AuthServ.authRole('USER'), _.routeAsync(a
     const limit = _.parseIntNull(req.query.limit) || 50;
     const offset = _.parseIntNull(req.query.offset) || Number.MAX_SAFE_INTEGER;
 
-    const promotions = await Promotion.find<IPromotion>({ $limit: limit, $skip: offset, status: PROMOTION_STATUSES.ENABLED }).toArray();
+    const promotions = await Promotion.find<IPromotion>({ status: PROMOTION_STATUSES.ENABLED }, { limit: limit, skip: offset }).toArray();
     return promotions;
 }));
 
